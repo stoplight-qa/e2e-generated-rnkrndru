6a7bc8f517969b25cb0a28fcd3a650dcf84c479d
qa-acdii-0-jejx@example.com <qa-acdii-0-jejx@example.com>
2023-04-05 14:13:44 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15889.stoplight-dev.com/api/v1/projects/cHJqOjE3MDI/spectral.js?branch_slug=main&token=0736d569-cb64-42fb-b63f-6455ced314da";
+export default { extends: ruleset };
